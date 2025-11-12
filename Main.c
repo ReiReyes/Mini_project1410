b2436651e7ddda4680dcfb8e4f4e114cc600dd6f
ReiReyes <reinaldoreyes02@gmail.com>
2025-11-12 09:08:55 -0500

    new

--- Main.c
+++ Main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
 int main(){
-
+    int option;
+    printf("Welcome to the Tic-Tac-Toe Game\n");
+    while(1){
+        printf("(1) Player vs Player");
+        printf("\n(2) Player vs AI");
+        printf("\n(3) Exit the game");
+        printf("\nSelect the option you would like: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            
+            break;
+        
+        case 2:
+            
+            break;
+        case 3:
+            printf("\nThamk You for playing");
+            break;
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
